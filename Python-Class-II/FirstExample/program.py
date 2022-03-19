32ef418863c92d9e7c61fc564965f1a6dc01f20b
Humaiun Kabir <humaiunkabir@outlook.com>
2022-03-19 22:09:43 +0600

    Smart Door App Completed.

---
+++ Python-Class-II/FirstExample/program.py
@@ -0,0 +1,19 @@
+#input phase
+name = input("Enter Your Name:")
+
+#process phase
+name = name + "!"
+
+# Output phase-> Name!
+print(name)
+
+
+#input phase
+fname = input("Enter Your First Name:")
+lname = input("Enter Your Last Name:")
+
+#process phase
+fullname = fname + "@" + lname
+
+# Output phase-> Name!
+print(fullname)
---
+++ Python-Class-II/FirstExample/test.py
@@ -0,0 +1,4 @@
+name="John"
+age=26
+weight=66.66
+print("Weight: %.2f Age: %d Name: %s" %(weight, age, name))
